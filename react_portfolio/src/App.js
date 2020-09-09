e27aeedab3f8c57a43c3b880a23b09aca9fd2d71
rudy gauna <rudy.gauna@gmail.com>
2020-09-08 19:11:18 -0500

    initial commit

---
+++ react_portfolio/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="App-header">
+        <h1>No worries its all good!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
